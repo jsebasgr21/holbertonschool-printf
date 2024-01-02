687ba67b9825d42d6e5854ef046335580d7be29f
jsebasgr21 <jsebasgr21@hotmail.com>
2024-01-02 03:14:50 -0500

    fix indent

--- print_f.c
+++ print_f.c
@@ -3,11 +3,10 @@
 int _printf(const char *format, ...)
 {
 int nchars = 0;
-
 va_list list_of_args;
 
 if(format == NULL)
-    return (-1);
+return (-1);
 
 va_start(list_of_args, format);
 
@@ -15,39 +14,39 @@ while(*format)
 {
 if(*format != '%')
 {
-    write(1, format, 1);
-    nchars++;
+write(1, format, 1);
+nchars++;
 }
 else
 {
-    format++;
-    if(*format == '\0')
-        break;
+format++;
+if(*format == '\0')
+break;
 
-    if(*format == '%')
-    {
-        write(1, format, 1);
-        nchars++;
-    }
+if(*format == '%')
+{
+write(1, format, 1);
+nchars++;
+}
 
-    else if(*format == 'c')
-    {
-        char c = va_arg(list_of_args, int);
-        write(1, &c, 1);
-        nchars++;
-    }
+else if(*format == 'c')
+{
+char c = va_arg(list_of_args, int);
+write(1, &c, 1);
+nchars++;
+}
 
-    else if(*format == 's')
-    {
-        char *str = va_arg(list_of_args, char*);
-        int str_len = 0;
+else if(*format == 's')
+{
+char *str = va_arg(list_of_args, char*);
+int str_len = 0;
 
-        while(str[str_len] != '\0')
-        str_len++;
+while(str[str_len] != '\0')
+str_len++;
 
-        write(1, str, str_len);
-        nchars += str_len;
-    }
+write(1, str, str_len);
+nchars += str_len;
+}
 }
 
 format++;
