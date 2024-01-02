cfef4d731536f495e492f217c866d9778d8dbe8e
jsebasgr21 <jsebasgr21@hotmail.com>
2024-01-02 10:34:24 -0500

    add if is null

--- print_f.c
+++ print_f.c
@@ -5,14 +5,14 @@ int _printf(const char *format, ...)
 int nchars = 0;
 va_list list_of_args;
 
-if(format == NULL)
+if (format == NULL)
 return (-1);
 
 va_start(list_of_args, format);
 
-while(*format)
+while (*format)
 {
-if(*format != '%')
+if (*format != '%')
 {
 write(1, format, 1);
 nchars++;
@@ -20,28 +20,33 @@ nchars++;
 else
 {
 format++;
-if(*format == '\0')
+if (*format == '\0')
 break;
 
-if(*format == '%')
+if (*format == '%')
 {
 write(1, format, 1);
 nchars++;
 }
 
-else if(*format == 'c')
+else if (*format == 'c')
 {
 char c = va_arg(list_of_args, int);
 write(1, &c, 1);
 nchars++;
 }
 
-else if(*format == 's')
+else if (*format == 's')
 {
 char *str = va_arg(list_of_args, char*);
 int str_len = 0;
 
-while(str[str_len] != '\0')
+if (str == NULL)
+{
+str = "(null)";
+}
+
+while (str[str_len] != '\0')
 str_len++;
 
 write(1, str, str_len);
