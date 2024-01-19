70a287fa5895efd82a73bc7e74d577919d65f331
jsebasgr21 <jsebasgr21@hotmail.com>
2024-01-19 11:46:47 -0500

    test

--- functions.c
+++ functions.c
@@ -9,20 +9,24 @@ if (formato == '%')
 write(1, formato, 1);
 nchars++;
 }
+
 else if (formato == 'c')
 {
 char c = va_arg(list_of_args, int);
 write(1, &c, 1);
 nchars++;
 }
+
 else if (formato == 's')
 {
 char *str = va_arg(list_of_args, char*);
 int str_len = 0;
+
 if (str == NULL)
 {
 str = "(null)";
 }
+
 while (str[str_len] != '\0')
 str_len++;
 
